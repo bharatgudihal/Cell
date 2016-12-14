1e4ce4a9dda6a820c08e9b3d79039509f9446ec8
sbens <sbensharp@gmail.com>
2016-12-13 17:20:17 -0700

    Doors and Models
    Added all of gabes models and moved doors

---
+++ Assets/Scripts/DoorMotor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotor : MonoBehaviour {
+
+    public GameObject lDoor;
+    public GameObject rDoor;
+
+    public Transform rDoorOpen;//this is the a local transform for where the door 
+    public Transform rDoorClose;
+
+    public Transform lDoorOpen;//local transform for the girl to 
+    public Transform lDoorClose;
+
+    public bool doorOpen;
+    public bool activate;
+    public bool canUse = false;
+
+    public float doorSpeed=500f;
+	
+	
+    public void Update()
+    {
+
+        
+        if(Input.GetKeyDown(KeyCode.Q)&&canUse)
+        {
+            activate = true;
+            
+        }
+        if (!doorOpen && activate)
+        {
+            rDoor.transform.position = Vector3.Lerp(rDoor.transform.position, rDoorOpen.transform.position, doorSpeed * Time.deltaTime);
+            lDoor.transform.position = Vector3.Lerp(lDoor.transform.position, lDoorOpen.transform.position, doorSpeed * Time.deltaTime);
+            if (lDoor.transform.position == lDoorOpen.transform.position)
+            {
+                
+                activate = false;
+                doorOpen = true;
+            }      
+        }
+        if(doorOpen && activate)
+        {
+            
+            rDoor.transform.position = Vector3.Lerp(rDoor.transform.position, rDoorClose.transform.position, doorSpeed * Time.deltaTime);
+            lDoor.transform.position = Vector3.Lerp(lDoor.transform.position, lDoorClose.transform.position, doorSpeed * Time.deltaTime);
+            if (lDoor.transform.position == lDoorClose.transform.position)
+            {
+                
+                activate = false;
+                doorOpen = false;
+            }
+
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canUse = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canUse = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+    public GameObject spotLight;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            spotLight.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            spotLight.SetActive(false);
+        }
+    }
+}
