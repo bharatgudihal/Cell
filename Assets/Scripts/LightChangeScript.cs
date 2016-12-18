0e13a1fb66b6db62f806b4e9341bdd8fcb8f98fa
sbens <sbensharp@gmail.com>
2016-12-17 18:26:19 -0700

    Main menu Restart logic

---
+++ Assets/Scripts/LightChangeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChangeScript : MonoBehaviour {
+
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            rend.material.SetColor("_Color", Color.blue);
+            rend.material.SetColor("_EmissionColor", Color.blue);
+
+        }
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class MainMenu : MonoBehaviour {
+
+    public EventSystem es;
+    private GameObject storeSelected;
+
+
+    public void Start()
+    {
+        storeSelected = es.firstSelectedGameObject;
+    }
+    private void Update()
+    {
+        if(es.currentSelectedGameObject != storeSelected)
+        {
+            if(es.currentSelectedGameObject == null)
+            {
+                es.SetSelectedGameObject(storeSelected);
+            }
+            else
+            {
+                storeSelected = es.currentSelectedGameObject;
+            }
+
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("WhiteBox", LoadSceneMode.Single);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
